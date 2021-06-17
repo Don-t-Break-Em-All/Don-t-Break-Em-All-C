ba277d15666f8a217d43344c11d37eaaad62c611
Adam Salhi <adam06genius@gmail.com>
2021-06-17 19:00:34 +0100

    Initial Commit

---
+++ dba.c
@@ -0,0 +1,34 @@
+#include "raylib.h"
+
+int main(void) {
+    const int screenWidth = 320;
+    const int screenHeight = 200;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
+
+    Vector2 playerPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+
+    SetTargetFPS(60); 
+
+    Texture2D player = LoadTexture("assets/character1.png");              
+
+    while (!WindowShouldClose()) {
+        if (IsKeyDown(KEY_RIGHT)) playerPosition.x += 2.0f;
+        if (IsKeyDown(KEY_LEFT)) playerPosition.x -= 2.0f;
+        if (IsKeyDown(KEY_UP)) playerPosition.y -= 2.0f;
+        if (IsKeyDown(KEY_DOWN)) playerPosition.y += 2.0f;
+
+        BeginDrawing();
+
+            ClearBackground(BLACK);
+
+            DrawTexture(player, playerPosition.x, playerPosition.y, WHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();        
+    
+
+    return 0;
+}
